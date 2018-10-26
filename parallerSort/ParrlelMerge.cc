80b83267485fae9acab5e72de03ef6afcd7452bd
liad <liad.abenzour64@gmail.com>
2018-10-26 14:18:32 +0300

    fin version

--- parallerSort/ParrlelMerge.cc
+++ parallerSort/ParrlelMerge.cc
@@ -1,6 +1,8 @@
 #include "pch.h"
 #include <math.h> 
 #include <iostream>
+#include <memory>
+#include <algorithm>
 #include "ParrlelMerge.h"
 
 ParrlelMerge::ParrlelMerge(size_t threads)
@@ -22,39 +24,37 @@ void ParrlelMerge::merge(std::vector<size_t>& in, size_t start, size_t end)
 	std::copy(temp.begin() + start, temp.begin() + end, in.begin() + start);
 	std::cout << "start: " << start << " end: " << end << std::endl;
 	print_merged(in);
-	//dsfds
 }
 
 auto ParrlelMerge::add_task(std::vector<size_t>& in, size_t start, size_t end, size_t level) -> ParrlelMerge::node_t&
 {
 	if (end <= start + 1) {
-		auto rv = std::make_unique<node_t>(m_g, [](msg_t) {
-			std::cout << "leaf start" << std::endl;
-		});
+		std::unique_ptr<node_t>rv(new node_t(m_g, [](msg_t) {
+		}));
 		tbb::flow::make_edge(m_root_node, *rv);
 		m_nodes.emplace_back(std::move(rv));
 		return  *m_nodes.back();
 	}
-	/*
+	
 	if (level > log2(m_threads)*2) {
-		auto rv = std::make_unique<node_t>(m_g, [&in, start, end](msg_t) {
+		std::unique_ptr<node_t>rv(new node_t(m_g, [&in, start, end](msg_t) {
 			std::sort(in.begin() + start, in.begin() + end);
-		});
+		}));
 		tbb::flow::make_edge(m_root_node, *rv);
 		m_nodes.emplace_back(std::move(rv));
 		return  *m_nodes.back();
 	}
-	*/
+	
 	int m = start + (end - start) / 2;
 	auto& l = add_task(in, start, m, level + 1);
 	auto& r = add_task(in, m, end, level + 1);
 
-	auto rv = std::make_unique<node_t>(m_g, [&in, start, end, m](msg_t) {
+	std::unique_ptr<node_t>rv(new node_t(m_g, [&in, start, end, m](msg_t) {
 		//std::cout << "merger task start" << std::endl;
-		std::vector<size_t> temp = in;
-		std::merge(in.begin() + start, in.begin() + m, in.begin() + m, in.begin() + end, temp.begin() + start);
-		std::copy(temp.begin() + start, temp.begin() + end, in.begin() + start);
-	});
+		std::vector<size_t> temp(end - start);
+		std::merge(in.begin() + start, in.begin() + m, in.begin() + m, in.begin() + end, temp.begin());
+		std::copy(temp.begin(), temp.begin() + end - start, in.begin() + start);
+	}));
 
 	tbb::flow::make_edge(l, *rv);
 	tbb::flow::make_edge(r, *rv);
@@ -64,13 +64,13 @@ auto ParrlelMerge::add_task(std::vector<size_t>& in, size_t start, size_t end, s
 
 void ParrlelMerge::par_sort(std::vector<size_t>& in)
 {
-	std::cout << "#########################################start#########################################" << std::endl;
-	print_merged(in);
+	//std::cout << "#########################################start#########################################" << std::endl;
+	//print_merged(in);
 	auto fin = add_task(in, 0, in.size(), 0);
 	m_root_node.try_put(tbb::flow::continue_msg());
 	m_g.wait_for_all();
-	print_merged(in);
-	std::cout << "#########################################end#########################################" << std::endl;
+	//print_merged(in);
+	//std::cout << "#########################################end#########################################" << std::endl;
 }
 
 void ParrlelMerge::print_merged(std::vector<size_t> in) const
--- parallerSort/ParrlelMerge.h
+++ parallerSort/ParrlelMerge.h
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <memory>
 #include "tbb/flow_graph.h"
 
 class ParrlelMerge
